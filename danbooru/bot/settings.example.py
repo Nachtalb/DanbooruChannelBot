2f172ead590d9293a1f68389b9feaba39b0c59fa
Nachtalb <info@nachtalb.io>
2020-07-20 18:35:53 +0300

    Set all settings via environment variables

--- danbooru/bot/settings.example.py
+++ danbooru/bot/settings.example.py
@@ -1,36 +1,55 @@
 import logging
 import os
 
-TELEGRAM_API_TOKEN = os.environ['TELEGRAM_API_TOKEN']
 
-ADMINS = os.environ['ADMINS']
-CHAT_ID = int(os.environ['CHAT_ID'])
+def env(name, default=None, required=False, is_bool=False, is_list=False):
+    value = os.environ.get(name)
+    if required and not name:
+        raise KeyError(f'You need to define the environmental variable "{name}"')
+    elif is_list or isinstance(default, (list, set, tuple)):
+        convert = type(default) if default is not None else list
+        if value is None:
+            return convert() if default is None else default
+        return convert(map(str.strip, value.split(',')))
+    elif is_bool or isinstance(default, bool):
+        if value is None:
+            return default or False
+        return bool(value.lower() in ['1', 'true', 'yes'])
+    elif value is None:
+        return default
+    return value
 
-SUFFIX = ''
+TELEGRAM_API_TOKEN = env('TELEGRAM_API_TOKEN', required=True)
 
-AUTO_START = True
-SHOW_CHARACTER_TAG = True
-SHOW_ARTIST_TAG = True
-SHOW_ID = True
+ADMINS = env('ADMINS', required=True, is_list=True)
+CHAT_ID = int(env('CHAT_ID', required=True))
 
-SEARCH_TAGS = 'rating:safe'  # AND filter
-POST_TAG_FILTER = set()      # OR filter
-MAX_TAGS = 15
-SHOWN_TAGS = {  # Tags that will always be shown. Other tags will be selected randomly to reach MAX_TAGS
+SUFFIX = env('SUFFIX', '')
+
+AUTO_START = env('AUTO', True)
+SHOW_CHARACTER_TAG = env('SHOW_CHARACTER_TAG', True)
+SHOW_ARTIST_TAG = env('SHOW_ARTIST_TAG', True)
+SHOW_ID = env('SHOW_ID', True)
+
+SEARCH_TAGS = env('SEARCH_TAGS', 'rating:safe') # AND filter
+POST_TAG_FILTER = env('POST_TAG_FILTER', set()) # OR filter
+MAX_TAGS = int(env('MAX_TAGS', 15))
+SHOWN_TAGS = env('SHOWN_TAGS', {  # Tags that will always be shown. Other tags will be selected randomly to reach MAX_TAGS
     '1girl', '2girls', '3girls', '4girls', '5girls', '6+girls', 'highres',
     'blue_eyes', 'blonde_hair', 'yuri'
-}
+})
 
-RELOAD_INTEVAL = 5  # in min
+RELOAD_INTEVAL = int(env('RELOAD_INTEVAL', 5))  # in min
 
 SERVICE = {
     'name': 'danbooru',
     'type': 'danbooru',
     'url': 'https://danbooru.donmai.us',
-    'api': os.environ['DANBOORU_API'],
-    'username': os.environ['DANBOORU_USERNAME'],
-    'password': os.environ['DANBOORU_PASSWORD'],
+    'api': env('DANBOORU_API'),
+    'username': env('DANBOORU_USERNAME'),
+    'password': env('DANBOORU_PASSWORD'),
 }
+
 # {
 #     'name': 'safebooru',
 #     'type': 'danbooru',
@@ -43,14 +62,20 @@ SERVICE = {
 
 # More information about polling and webhooks can be found here:
 # https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks
-MODE = {
-    'active': 'webhook',  # "webook" or "polling"
-    'configuration': {
-        'listen': '127.0.0.1',
-        'port': 5000,
-        'url_path': TELEGRAM_API_TOKEN,
-        'url': 'https://%s/%s' % (os.environ['DOMAIN'], TELEGRAM_API_TOKEN),
-    },
-}
+if env('WEBHOOK', True):
+    DOMAIN = env('DOMAIN', required=True)
+    MODE = {
+        'active': 'webhook',  # "webook" or "polling"
+        'configuration': {
+            'listen': '127.0.0.1',
+            'port': 5000,
+            'url_path': TELEGRAM_API_TOKEN,
+            'url': 'https://%s/%s' % (DOMAIN, TELEGRAM_API_TOKEN),
+        },
+    }
+else:
+    MODE = {
+        'active': 'polling',
+    }
 
 LOG_LEVEL = logging.DEBUG
