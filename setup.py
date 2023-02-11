5259ff80be4fd02bcf06601a89af49c6fefdc7df
Nachtalb <na@nachtalb.io>
2023-02-11 21:57:22 +0100

    Remove buildout stuff

--- setup.py
+++ setup.py
@@ -1,44 +1,40 @@
 from setuptools import find_packages, setup
 
-version = '1.1.2.dev0'
+version = "1.1.2.dev0"
 
-setup(name='DanbooruChannelBot',
-      version=version,
-      description="I post stuff uploaded to danbooru to a channel.",
-      long_description=f'{open("README.rst").read()}\n{open("CHANGELOG.rst").read()}',
-
-      author='Nachtalb',
-      url='https://github.com/Nachtalb/DanbooruChannelBot',
-      license='GPL3',
-
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['danbooru'],
-      include_package_data=True,
-      zip_safe=False,
-
-      install_requires=[
-          'mr.developer',
-          'python-telegram-bot',
-          'pybooru',
-          'requests_html',
-          'emoji',
-          'ffprobe-python',
-          'ffmpeg-python',
-          'Cython',
-          'timeout-decorator',
-          'python-dotenv',
-          'pyvips',
-          'yarl',
-      ],
-      extras_require={
-          'dev': [
-              'ipython',
-              'ipdb',
-          ]
-      },
-
-      entry_points={
-          'console_scripts': [
-              'bot = danbooru.bot.bot:main',
-          ]
-      })
+setup(
+    name="DanbooruChannelBot",
+    version=version,
+    description="I post stuff uploaded to danbooru to a channel.",
+    long_description=f'{open("README.rst").read()}\n{open("CHANGELOG.rst").read()}',
+    author="Nachtalb",
+    url="https://github.com/Nachtalb/DanbooruChannelBot",
+    license="GPL3",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["danbooru"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "python-telegram-bot==13.6",
+        "pybooru",
+        "requests_html",
+        "emoji",
+        "ffprobe-python",
+        "ffmpeg-python",
+        "timeout-decorator",
+        "python-dotenv",
+        "pyvips",
+        "yarl",
+    ],
+    extras_require={
+        "dev": [
+            "ipython[black]",
+            "ipdb",
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            "bot = danbooru.bot.bot:main",
+        ]
+    },
+)
