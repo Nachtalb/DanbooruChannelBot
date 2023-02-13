8a296fdad875b1606236fe20372a3ba58b66831e
Nachtalb <na@nachtalb.io>
2023-02-13 17:32:25 +0100

    Be sure all tags work on telegram

--- danbooru/utils.py
+++ danbooru/utils.py
@@ -1,5 +1,6 @@
+import random
 import re
-from typing import Iterable
+from typing import Iterable, Sequence
 
 from .models import ChatConfig, Post
 
@@ -8,15 +9,22 @@ def post_format(config: ChatConfig, post: Post) -> str:
     return config.template.format(
         posted_at=post.created_at.strftime("%d/%m/%Y at %H:%M"),
         id=post.id,
-        tags=", ".join(tg_tags(post.tags)),
+        tags=", ".join(sample(tg_tags(post.tags), 15)),
         artists=", ".join(tg_tags(post.tags_artist)),
         characters=", ".join(tg_tags(post.tags_character)),
     )
 
 
-def tg_tag(tag: str) -> str:
-    return ("#" if not tag[0] == "#" else "") + re.sub(r"[\(\)\-:]", "_", tag).replace("__", "_").strip("_")
+def sample(population: Sequence, k: int) -> Sequence:
+    return random.sample(population, k=len(population) if len(population) < k else k)
 
 
-def tg_tags(tags: Iterable[str]) -> set[str]:
-    return {tg_tag(tag) for tag in tags}
+def tg_tag(tag: str) -> str | None:
+    tag = re.sub("_{2,}", "", re.sub(r"\W", "_", tag)).strip("_")
+    if tag:
+        return ("#" if not tag[0] == "#" else "") + tag
+    return
+
+
+def tg_tags(tags: Iterable[str]) -> Sequence[str]:
+    return tuple(filter(None, map(tg_tag, tags)))
