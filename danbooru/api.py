859812a8308b5bd94c62136c95ffd3d9d4425ea1
Nachtalb <na@nachtalb.io>
2023-02-13 13:40:14 +0100

    Simplify danbooru api

--- danbooru/api.py
+++ danbooru/api.py
@@ -4,20 +4,15 @@ from .models import Post
 
 
 class Api:
-    def __init__(self, session: ClientSession, user: str, key: str):
-        self.session = session
-        self._auth = BasicAuth(user, key)
+    def __init__(self, user: str, key: str):
+        self.session = ClientSession(base_url="https://danbooru.donmai.us/", auth=BasicAuth(user, key))
         self.user = user
         self.key = key
 
-    async def _request(self, endpoint: str, params: dict = {}) -> dict:
-        async with self.session.get(
-            f"https://danbooru.donmai.us/{endpoint}.json", params=params, auth=self._auth
-        ) as response:
-            return await response.json()
-
     async def posts(self) -> list[Post]:
-        return [Post.parse_obj(item) for item in await self._request("posts")]
+        async with self.session.get("posts.json") as response:
+            return [Post.parse_obj(item) for item in await response.json()]
 
     async def post(self, id: int) -> Post:
-        return Post.parse_obj(await self._request(f"posts/{id}"))
+        async with self.session.get(f"posts/{id}.json") as response:
+            return Post.parse_obj(await response.json())
