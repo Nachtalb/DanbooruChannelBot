2c41a6cb96bfadf212a854890035eb58ce5df7e7
Nachtalb <na@nachtalb.io>
2023-02-13 15:03:14 +0100

    Set chat config via context

--- danbooru/cli.py
+++ danbooru/cli.py
@@ -1,7 +1,15 @@
-from asyncio import run
-
 from telegram import Update
-from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (
+    Application,
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+
+from danbooru.models.chat_config import ChatConfig
+from danbooru.models.post import Post
 
 from .api import Api
 from .commands import help, settings, start
@@ -10,23 +18,18 @@ from .models import Config
 api: Api
 
 
-async def test_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global api
-    if not update.message:
-        return
-    post = (await api.posts(1))[0]
-    await update.message.reply_photo(post.best_file_url, caption=f"by {post.tag_string_artist}")
-
-
 async def post_init(application: Application):
     await application.bot.set_my_commands(
         [
             ("start", "Starts the bot"),
             ("help", "Show help message"),
             ("settings", "Configure the bot"),
-            ("test_command", "Run test command"),
         ]
     )
+
+
+async def set_config(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    context.chat_data.setdefault("config", ChatConfig())  # type: ignore
 
 
 def main():
@@ -35,10 +38,10 @@ def main():
     application = ApplicationBuilder().post_init(post_init).token(config.BOT_TOKEN).build()
     api = Api(user=config.DANBOORU_USERNAME, key=config.DANBOORU_KEY)
 
+    application.add_handler(MessageHandler(filters.ALL, set_config), group=-1)
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("settings", settings))
-    application.add_handler(CommandHandler("test_command", test_command))
 
     if not config.WEBHOOK:
         application.run_polling()
