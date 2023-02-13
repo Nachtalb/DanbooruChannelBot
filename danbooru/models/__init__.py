a26fb010a2a2a7c1dc0bee197f0adbfce0254dd1
Nachtalb <na@nachtalb.io>
2023-02-13 13:42:30 +0100

    Split up models

---
+++ danbooru/models/__init__.py
@@ -0,0 +1,2 @@
+from .config import Config
+from .post import Post
--- danbooru/models/post.py
+++ danbooru/models/post.py
@@ -1,23 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel, BaseSettings
-
-
-class Config(BaseSettings):
-    BOT_TOKEN: str
-
-    WEBHOOK: bool
-    WEBHOOK_LISTEN: str = "0.0.0.0"
-    WEBHOOK_PORT: int = 80
-    WEBHOOK_EXT_DOMAIN: str | None
-    WEBHOOK_EXT_PATH: str | None
-
-    DANBOORU_USERNAME: str
-    DANBOORU_KEY: str
-
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+from pydantic import BaseModel
 
 
 class Post(BaseModel):
