fcdf50a1c97ffbe138c362d6e1d8953f72e2a727
Nachtalb <na@nachtalb.io>
2023-02-13 18:44:43 +0100

    Add inline buttons

--- danbooru/handlers/poster.py
+++ danbooru/handlers/poster.py
@@ -1,8 +1,6 @@
-from asyncio import create_task, gather, to_thread
-
-from PIL import Image
-from telegram import Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes
+from yarl import URL
 
 from danbooru import app
 from danbooru.files import image
@@ -23,10 +21,18 @@ async def post(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     else:
         posts = await app.api.posts(2)
 
-    for task in [_prepare_post(context.chat_data["config"], post) for post in posts]:  # type: ignore
+    config = context.chat_data["config"]  # type: ignore
+    for task, post in [(_prepare_file(config, post), post) for post in posts]:
         data = await task
         if not data:
             continue
+
+        data.update(
+            {
+                "caption": post_format(config, post),
+                "reply_markup": _get_markup(config, post),
+            }
+        )
 
         await update.message.reply_text(data["filename"])
         if "photo" in data:
@@ -37,26 +43,39 @@ async def post(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             await update.message.reply_document(**data)
 
 
-async def _prepare_post(config: ChatConfig, post: Post) -> dict | None:
+def _get_markup(chat: ChatConfig, post: Post):
+    buttons = []
+    if chat.show_danbooru_button:
+        buttons.append(InlineKeyboardButton("📦", post.url))
+    if chat.show_source_button and post.source and not post.source.startswith("file:"):
+        match URL(post.source):
+            case url if url.host in ["twitter.com", "t.co"]:
+                emoji = "🐦"
+            case url if url.host in ["pixiv.net"]:
+                emoji = "🅿️"
+            case _:
+                emoji = "🌐"
+        buttons.append(InlineKeyboardButton(emoji, post.source))
+    if chat.show_direct_button:
+        buttons.append(InlineKeyboardButton("🖼️", post.best_file_url))
+    return InlineKeyboardMarkup([buttons])
+
+
+async def _prepare_file(config: ChatConfig, post: Post) -> dict | None:
     if post.is_removed or not config.post_allowed(post):
         return
 
-    caption = post_format(config, post)
     file = None
 
     if post.is_image:
         file, filename, as_document = await image.ensure_tg_compatibility(post)
         if not as_document:
-            return {"photo": file, "filename": filename, "caption": caption}
+            return {"photo": file, "filename": filename}
 
     if post.is_video or post.is_gif:
         return {
             "video": post.best_file_url,
             "filename": post.filename,
-            "caption": caption,
         }
 
-    return {
-        "document": file or await app.api.download(post.best_file_url),
-        "caption": caption,
-    }
+    return {"document": file or await app.api.download(post.best_file_url), "filename": post.filename}
