7073a6dc2a54aed9a864f693ebf10873aa29abe1
Nachtalb <na@nachtalb.io>
2023-02-14 16:06:48 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -15,16 +15,12 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "emoji==2.2.0",
-        "ffmpeg-python==0.2.0",
-        "ffprobe-python==1.0.3",
-        "Pybooru==4.2.2",
-        "python-dotenv==0.21.1",
-        "python-telegram-bot==13.15",
-        "pyvips==2.2.1",
-        "requests-html==0.10.0",
-        "timeout-decorator==0.5.0",
-        "yarl==1.8.2",
+        "aiohttp[speedups]=<3.0",
+        "aiopath=<0.7",
+        "emoji=<2.3",
+        "Pillow=<9.5",
+        "pydantic[dotenv]=<1.11",
+        "python-telegram-bot[rate-limiter]=<21",
     ],
     extras_require={
         "dev": [
